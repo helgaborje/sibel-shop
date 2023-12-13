18f80242e3f785eb673629c5b8bd50ef78a676bc
Helga Börjesson <helgaborjesson@gmail.com>
2023-12-13 09:58:35 +0100

    added product cards to shop with dummy data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from '../components/navigation/navigation.component';
+import { ShopComponent } from '../components/shop/shop.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavigationComponent],
+  imports: [CommonModule, RouterOutlet, NavigationComponent, ShopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/components/shop/shop.component.ts
+++ src/components/shop/shop.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Product } from '../../types/types';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss'
 })
-export class ShopComponent {
+export class ShopComponent implements OnInit {
+  products: Product[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<Product[]>('./assets/products.json').subscribe(response => {
+      console.log(response)
+      this.products = response;
+    });
+  }
 }
