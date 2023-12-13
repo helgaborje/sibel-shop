dd848a18fd25e8e8fdbcba8772ed2cd907300d3c
Helga Börjesson <helgaborjesson@gmail.com>
2023-12-13 11:20:03 +0100

    added a offcanvas for cart

--- src/components/navigation/navigation.component.ts
+++ src/components/navigation/navigation.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CartComponent } from '../cart/cart.component';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [],
+  imports: [CartComponent],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
 })
