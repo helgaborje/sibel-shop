46b0374a21aec8534590b8fa063ce22f75171416
Helga Börjesson <helgaborjesson@gmail.com>
2023-12-15 09:46:25 +0100

    added a form for check out

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from '../components/home/home.component';
 import { ShopComponent } from '../components/shop/shop.component';
 import { PageNotFoundComponent } from '../components/page-not-found/page-not-found.component';
 import { ProductComponent } from '../components/product/product.component';
+import { CheckOutComponent } from '../components/check-out/check-out.component';
 
 export const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'products', component: ShopComponent},
-  {path: 'products/:id', component: ProductComponent},
+  { path: 'products/:id', component: ProductComponent },
+  {path: 'checkout', component: CheckOutComponent},
   {path: '**', component: PageNotFoundComponent}
 ];
 
--- src/components/cart/cart.component.ts
+++ src/components/cart/cart.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss'
 })
 export class CartComponent {
 
+  checkOut() {
+
+  }
 }
