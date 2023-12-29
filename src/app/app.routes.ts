c5db0eb6dd699869221c748aa34a837162f8fad5
Helga Börjesson <helgaborjesson@gmail.com>
2023-12-29 11:51:49 +0100

    added a route for admin and a login form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,10 @@ import { PageNotFoundComponent } from '../components/page-not-found/page-not-fou
 import { ProductComponent } from '../components/product/product.component';
 import { CheckOutComponent } from '../components/check-out/check-out.component';
 import { ProductPageComponent } from '../components/product-page/product-page.component';
+import { AdminComponent } from '../components/admin/admin.component';
 
 export const routes: Routes = [
+  {path: 'admin', component: AdminComponent},
   {path: 'home', component: HomeComponent},
   {path: 'products', component: ShopComponent},
   { path: 'product/:id', component: ProductPageComponent },
