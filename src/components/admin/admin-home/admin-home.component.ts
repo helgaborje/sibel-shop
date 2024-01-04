3fab0c8bd874622b4e25e23618629808a9b4129e
Helga Börjesson <helgaborjesson@gmail.com>
2024-01-04 10:09:04 +0100

    added ng material tab for orders and products

--- src/components/admin/admin-home/admin-home.component.ts
+++ src/components/admin/admin-home/admin-home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
 import { Router } from '@angular/router';
 import { OrdersComponent } from '../../orders/orders.component';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @Component({
   selector: 'app-admin-home',
   standalone: true,
-  imports: [OrdersComponent],
+  imports: [OrdersComponent, MatTabsModule],
   templateUrl: './admin-home.component.html',
   styleUrl: './admin-home.component.scss'
 })
