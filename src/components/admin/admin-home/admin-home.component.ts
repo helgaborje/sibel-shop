ab9de04243bd60fccf61209e2dcfa80d86f5e842
Helga Börjesson <helgaborjesson@gmail.com>
2024-01-04 09:49:45 +0100

    set pending to true or false on click, two separate tables

--- src/components/admin/admin-home/admin-home.component.ts
+++ src/components/admin/admin-home/admin-home.component.ts
@@ -1,30 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
 import { Router } from '@angular/router';
-import { Order } from '../../../types/types';
 import { CommonModule } from '@angular/common';
 import { OrderService } from '../../../services/order.service';
-import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-admin-home',
   standalone: true,
-  imports: [CommonModule, MatTableModule],
+  imports: [CommonModule, MatTableModule, MatIconModule],
   templateUrl: './admin-home.component.html',
   styleUrl: './admin-home.component.scss'
 })
 export class AdminHomeComponent implements OnInit{
-  orders: Order[] = [];
-  dataSource!: MatTableDataSource<any>;
-  displayedColumns: string[] =  ['position','name', 'products', 'total'];
-
+  displayedColumns: string[] =  ['position','name', 'products', 'total', 'check' ];
+  newOrders: any[] = [];
+  handledOrders: any[] = [];
 
 
   constructor(
     private authService: AuthService,
     private orderService: OrderService,
     private router: Router
-    ) { }
+  ) { }
+
+  private pendingOrders() {
+    this.orderService.getOrders().subscribe((orders) => {
+      this.newOrders = orders.filter(order => order.pending);
+      this.handledOrders = orders.filter(order => !order.pending);
+    });
+  }
+
+  ngOnInit() {
+    this.pendingOrders();
+  }
 
   logout() {
     this.authService.logout()
@@ -37,11 +47,19 @@ export class AdminHomeComponent implements OnInit{
       });
   }
 
-  ngOnInit() {
-    this.orderService.getOrders().subscribe((orders) => {
-      this.orders = orders;
-      this.dataSource = new MatTableDataSource(this.orders);
-      console.log('Orders', orders);
+  // Set pending to false
+  handleOrder(order: any) {
+    order.pending = false;
+    this.orderService.updateOrder(order).subscribe(() => {
+      this.pendingOrders();
+    });
+  }
+
+  // Set pending to true
+  cancelOrder(order: any) {
+    order.pending = true;
+    this.orderService.updateOrder(order).subscribe(() => {
+      this.pendingOrders();
     });
   }
 
--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Firestore, collection, addDoc, DocumentData, DocumentReference, collectionData } from '@angular/fire/firestore';
+import { Firestore, collection, addDoc, DocumentData, DocumentReference, collectionData, doc, updateDoc } from '@angular/fire/firestore';
 import { Observable, from, map } from 'rxjs';
 import { Order } from '../types/types';
 
@@ -20,5 +20,11 @@ export class OrderService {
   getOrders(): Observable<any[]> {
     const ordersCollection = collection(this.fs, 'orders');
     return collectionData(ordersCollection, { idField: 'id' });
+  }
+
+
+  updateOrder(order: Order): Observable<void> {
+    console.log('order', order);
+    return from(updateDoc(doc(this.fs, `orders/${order.id}`), order));
   }
 }
